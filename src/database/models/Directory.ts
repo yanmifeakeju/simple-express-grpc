7e624fc9a8601521ec28863c3a4c0f8e03130370
yanmifeakeju <yanmifeakeju@gmail.com>
2022-06-09 18:28:07 +0100

    directory schema

--- src/database/models/Directory.ts
+++ src/database/models/Directory.ts
@@ -1,5 +1,44 @@
 import { Schema } from 'mongoose';
 
-const Directory = new Schema({});
+const Directory = new Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+
+  email: String,
+  phone: String,
+
+  address: {
+    houseNumber: {
+      type: String
+    },
+
+    street: {
+      type: String,
+      required: true
+    },
+
+    city: {
+      type: String,
+      required: true
+    },
+
+    state: {
+      type: String,
+      required: true
+    },
+
+    country: {
+      type: String,
+      required: true,
+      default: 'Nigeria'
+    },
+
+    description: {
+      type: String
+    }
+  }
+});
 
 export default Directory;
